21342a848cc0fccf946ab548daaaf67c03a61bf4
JIElite <ita3051@gmail.com>
2015-10-16 13:45:21 -0400

    format *.c using astyle

--- q2/iterative.c
+++ q2/iterative.c
@@ -5,10 +5,12 @@
 
 char smallest_char(char str[], char c)
 {
-    for(int i = 0; i < strlen(str); i++){
+    for(int i = 0; i < strlen(str); i++) {
         /* if the restrict bigger character is found, set result */
-        if (str[i] == c){
-            while(str[i++] != '\0'){ if (str[i] > c) return str[i]; }
+        if (str[i] == c) {
+            while(str[i++] != '\0') {
+                if (str[i] > c) return str[i];
+            }
         }
     }
     /* if the restrict bigger character is not found */
@@ -19,13 +21,13 @@ int main()
 {
     char str[] = "cfjpv";
     char str2[] = "aabceeffffggwzzz";
- 
+
     unit_test(smallest_char, 'c', str, 'a');
     unit_test(smallest_char, 'f', str, 'c');
     unit_test(smallest_char, 'c', str, 'w');
     unit_test(smallest_char, 'p', str, 'j');
     unit_test(smallest_char, 'c', str, 'v');
-    
+
     unit_test(smallest_char, 'b', str2, 'a');
     unit_test(smallest_char, 'a', str2, 'd');
     unit_test(smallest_char, 'g', str2, 'f');
--- q2/recursive.c
+++ q2/recursive.c
@@ -3,10 +3,11 @@
 
 #include "unit_test.h"
 
- char find_smallest(char *str, char c, char result, int i){
-    if (str[i] != '\0'){
+char find_smallest(char *str, char c, char result, int i)
+{
+    if (str[i] != '\0') {
         if (str[i] == c) {
-            while(str[i++] != '\0'){
+            while(str[i++] != '\0') {
                 if (str[i] > c) {
                     result = str[i];
                     return result;
@@ -21,7 +22,7 @@
 
 
 char smallest_char_rev(char str[], char c)
-{   
+{
     if ( *str != '\0') return find_smallest(str, c, str[0], 0);
     return *str;
 }
@@ -31,17 +32,17 @@ int main()
 {
     char str[] = "cfjpv";
     char str2[] = "aabceeffffggwzzz";
- 
+
     unit_test(smallest_char_rev, 'c', str, 'a');
     unit_test(smallest_char_rev, 'f', str, 'c');
     unit_test(smallest_char_rev, 'c', str, 'w');
     unit_test(smallest_char_rev, 'p', str, 'j');
     unit_test(smallest_char_rev, 'c', str, 'v');
-    
+
     unit_test(smallest_char_rev, 'b', str2, 'a');
     unit_test(smallest_char_rev, 'a', str2, 'd');
     unit_test(smallest_char_rev, 'g', str2, 'f');
     unit_test(smallest_char_rev, 'e', str2, 'c');
-  
+
     return 0;
 }
