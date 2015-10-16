cfe18dbb4c91e35d47b436faa1399d215d484061
JIElite <ita3051@gmail.com>
2015-10-16 13:43:16 -0400

    remove #include in header file, define a function pointer as test_function type

--- q2/unit_test.c
+++ q2/unit_test.c
@@ -4,20 +4,21 @@
 #include "unit_test.h"
 
 
-void unit_test(char (*fptr)(char *, char), char result, char str[], char c){
-    
+void unit_test(test_function fptr, char result, char str[], char c)
+{
+
     printf("Pass Test: %c, Input: [", result);
-    
-    for(int i = 0; i < strlen(str); i++){
+
+    for(int i = 0; i < strlen(str); i++) {
         if(str[i+1] == '\0') printf("%c", str[i]);
         else printf("%c, ", str[i]);
     }
-    
+
     printf("], %c. --- ", c);
     char your_result = fptr(str, c);
 
     if (result == your_result) printf("Pass!\n");
     else printf("Not Pass!, Your result: %c\n", your_result);
 }
- 
+
 
--- q2/unit_test.h
+++ q2/unit_test.h
@@ -1,9 +1,9 @@
-#include <stdio.h>
-
 #ifndef UNIT_TEST
-#define UNIT_TEST 1
+#define UNIT_TEST
+
+typedef char (*test_function)(char *, char);
 void unit_test(char (*fptr)(char *, char), char result, char *str, char c);
 
-#endif 
+#endif
 
 
