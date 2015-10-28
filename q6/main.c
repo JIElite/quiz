0f130441284efa0cfc6fa13858614ad0c80bb260
JIElite <ita3051@gmail.com>
2015-10-28 21:40:08 +0800

    update main.c: add assert to check command line argument

--- q6/main.c
+++ q6/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "list.h"
 
@@ -15,38 +16,42 @@ extern struct ListNode *reverseBetween(struct ListNode*, int, int);
 
 int main(int argc, char *argv[]){
 
-    struct ListNode *head = create_list(1, 5);
-    
+    assert(argc == 2);
+
+    /* create a sequential list */
+    struct ListNode *head = create_list(1, 5); 
     printf("Initial list: ");
     traverse(head);
     
-
+    
+    /* run test case */
     int test_case = atoi(argv[1]);
+    assert(test_case > 0 && test_case <= 5);
     switch(test_case){
         case 1:
-            /* case */
+            /* case 1 */
             printf("reverse m = 2,  n = 3: ");
-            head = (struct ListNode*)reverseBetween(head, 2, 3);
+            head = reverseBetween(head, 2, 3);
             break;
         case 2:
             /* case 2 reverse full list */
             printf("reverse m = 1,  n = 5: ");
-            head = (struct ListNode*)reverseBetween(head, 1, 5);
+            head = reverseBetween(head, 1, 5);
             break;
         case 3:
             /* case 3 start position is head */
             printf("reverse m = 1,  n = 4: ");
-            head = (struct ListNode*)reverseBetween(head, 1, 4);
+            head = reverseBetween(head, 1, 4);
             break;
         case 4:
             /* case 4 end position is end of list */
             printf("reverse m = 3,  n = 5: ");
-            head = (struct ListNode*)reverseBetween(head, 3, 5);
+            head = reverseBetween(head, 3, 5);
             break;
         case 5:
             /* case 5 inside the list */
             printf("reverse m = 2,  n = 4: ");
-            head = (struct ListNode*)reverseBetween(head, 2, 4);
+            head = reverseBetween(head, 2, 4);
             break;
     }
     
